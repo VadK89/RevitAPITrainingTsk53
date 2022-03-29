693d19a3301cba2546c449f4a5bd29f7b44bfef4
Вадим <Vadimk89@mail.ru>
2022-03-29 23:33:04 +0500

    Добавьте файлы проекта.

---
+++ RevitAPITrainingTsk53/Main.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace RevitAPITrainingTsk53
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalApplication
+    {
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // имя вкладки   вревит
+            string tabName = "revit API Training Task5.3";
+            // создание вкладки
+            application.CreateRibbonTab(tabName);
+
+            //Путь к папке с приложением
+
+            string utilsFolderPath = @"D:\RevitAPITrainig\";
+
+
+            // создание панели
+
+            var panel = application.CreateRibbonPanel(tabName, "Задание 5.3");
+
+            //Создание кнопки
+
+            var button1 = new PushButtonData("Система", "Кнопки со свойствами",
+                Path.Combine(utilsFolderPath, "RevitAPITrainingTSk51.dll"),
+                "RevitAPITrainingTSk51.Main");
+            var button2 = new PushButtonData("Система", "Смена типов стен",
+                Path.Combine(utilsFolderPath, "RevitAPITrainingTSk52.dll"),
+                "RevitAPITrainingTSk52.Main");
+
+
+            //Изображение для кнопки
+            Uri uriImage1 = new Uri(@"D:\Учеба 2021\M3 tsks\T5\Tsk\RevitAPITrainingTsk53\RevitAPITrainingTsk53\Images\Proper.jpg", UriKind.Absolute);
+            BitmapImage largeImage1 = new BitmapImage(uriImage1);
+            button1.LargeImage = largeImage1;
+            Uri uriImage2 = new Uri(@"D:\Учеба 2021\M3 tsks\T5\Tsk\RevitAPITrainingTsk53\RevitAPITrainingTsk53\Images\walls.jpg", UriKind.Absolute);
+            BitmapImage largeImage2 = new BitmapImage(uriImage2);
+            button2.LargeImage = largeImage2;
+
+
+            //Добавление кнопки на панель
+
+            panel.AddItem(button1);
+            panel.AddItem(button2);
+
+            return Result.Succeeded;
+        }
+    }
+}
